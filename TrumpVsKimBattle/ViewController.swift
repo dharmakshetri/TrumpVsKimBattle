7423f3ce059eacef9c0cc11a0c2f2f7165558ac3
dharmakshetri <dharma.kshetri@gmail.com>
2017-08-16 19:12:14 -0500

    Initial Commit

---
+++ TrumpVsKimBattle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrumpVsKimBattle
+//
+//  Created by Dharma Kshetri on 8/16/17.
+//  Copyright © 2017 Dharma Kshetri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
